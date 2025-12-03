3311e2286a8b6b86afc5ad7914769336faba45c4
nermakov2903-lab <nermakov2903@gmail.com>
2025-12-03 21:22:20 +0700

    commit2

--- task4.py
+++ task4.py
@@ -1,3 +1,5 @@
+import random
+
 def array_to_int(arr):
     return int("".join(map(str, arr)))
 
@@ -26,11 +28,61 @@ def big_number_operation(a, b, op):
         return int_to_array(result)
 
 
+def generate_digits(n):
+    return [random.randint(0, 9) for _ in range(n)]
 
-if __name__ == "__main__":
-    a = [9,9,9]
-    b = [1]
 
-    print("Сумма:", big_number_operation(a, b, "add"))
-    print("Разность:", big_number_operation(a, b, "sub"))
+def task4_menu():
+    a = None
+    b = None
+    result = None
+
+    while True:
+        print("\n=== ЗАДАНИЕ 4 ===")
+        print("1. Ввести массивы вручную")
+        print("2. Сгенерировать массивы случайно")
+        print("3. Выполнить операцию")
+        print("4. Показать результат")
+        print("5. Назад")
+
+        choice = input("Выберите пункт: ")
+
+        # --- ввод вручную ---
+        if choice == "1":
+            a = list(map(int, input("Массив 1: ").split()))
+            b = list(map(int, input("Массив 2: ").split()))
+            result = None
+
+        # --- случайные ---
+        elif choice == "2":
+            len1 = int(input("Длина массива 1: "))
+            len2 = int(input("Длина массива 2: "))
+            a = generate_digits(len1)
+            b = generate_digits(len2)
+            print("A =", a)
+            print("B =", b)
+            result = None
+
+        # --- выполнение ---
+        elif choice == "3":
+            if a is None or b is None:
+                print("Сначала введите данные!")
+                continue
+
+            op = input("Операция (add/sub): ")
+            result = big_number_operation(a, b, op)
+            print("Операция выполнена.")
+
+        # --- вывод ---
+        elif choice == "4":
+            if result is None:
+                print("Нет результата!")
+            else:
+                print("Результат:", result)
+
+        elif choice == "5":
+            return
+
+        else:
+            print("Неверный пункт.")
 
